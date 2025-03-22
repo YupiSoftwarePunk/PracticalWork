9d6427db358b27f9b4fcc0229594a9b133d16351
Yupi <youngperchik2404@gmail.com>
2025-03-22 19:46:44 +0500

    Finished base methods and fixed all code with inteface and class

---
+++ Library.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    public class Library<T> : IMediaManager<T> where T : Media
+    {
+        List<T> media = new List<T>();
+        Dictionary<string, T> titleFinder = new Dictionary<string, T>();
+
+        public void Add(T item)
+        {
+            if (!titleFinder.ContainsKey(item.Title))
+            {
+                media.Add(item);
+                titleFinder.Add(item.Title, (T)item.Title);
+            }
+            else
+            {
+                throw new Exception("Элемент с таким именем уже существует");
+            }
+
+        }
+
+        public bool Remove(string title)
+        {
+            if (titleFinder.ContainsKey(title))
+            {
+                if (title != null)
+                {
+                    media.Remove((T)title);
+                    titleFinder.Remove(title);
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                throw new Exception("Элемента с таким именем не существует");
+            }
+        }
+
+        public T FindByTitle(string title)
+        {
+            if (titleFinder.ContainsKey(title))
+            {
+                return titleFinder[title];
+            }
+            else
+            {
+                throw new Exception("Элемента с таким именем не существует");
+            }
+        }
+
+        public IEnumerable<T> FilterByYear(int year)
+        {
+            return media.Where(i => i.YearPublished == year);
+        }
+
+        public IEnumerable<T> GetAllAvailable()
+        {
+            return media.Where(i => i.IsAvailable);
+        }
+    }
+}
--- obj/Debug/net8.0/Practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/Practice.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c95f9fa4de07a6d20dc38d0a9ef472cb8f24e1f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
